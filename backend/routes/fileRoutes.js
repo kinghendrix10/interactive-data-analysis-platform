aebe6ba2e50a599a1cca132f52317f30f3f03aef
Huleji Abraham Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-07-18 14:57:41 -0500

    Update fileRoutes.js

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,8 +1,10 @@
-// /backend/routes/fileRoutes.js
+// backend/routes/fileRoutes.js
 const express = require('express');
 const router = express.Router();
 const fileUploadController = require('../controllers/fileUploadController');
+const { authenticate } = require('../middleware/auth');
 
-router.post('/upload', fileUploadController.uploadFile);
+// POST /api/files/upload
+router.post('/upload', authenticate, fileUploadController.uploadFile, fileUploadController.processFile);
 
 module.exports = router;
